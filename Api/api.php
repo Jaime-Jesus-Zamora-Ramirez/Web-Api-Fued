11dcebd31932082687d271a01546aa98f769ff2c
Jaime-Jesus-Zamora-Ramirez <jaime.zamora.is@unipolidgo.edu.mx>
2023-08-17 23:33:08 -0500

    7 peticiones

--- Api/api.php
+++ Api/api.php
@@ -1,12 +1,17 @@
 <?php
-// api.php
 
-include('../conexion/conexion.php'); // conexicon a la bae de datos
+include('../conexion/conexion.php');
+header("Access-Control-Allow-Origin: http://localhost/dentist/api/api.php");
+header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS, DELETE, HEAD, REPORT");
+header("Access-Control-Allow-Headers: Content-Type");
 
-//----------------------METODO GET INICIO-------------------------//
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $sql = "SELECT id_dentista, titulo, descripcion, imagen_url FROM dentistas";
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Allow:");
+    header("HTTP/1.1 200 OK");
+    echo "Solicitud OPTIONS exitosa";
+    exit();
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $sql = "SELECT id_dentista, titulo, descripcion, correo_electronico FROM dentistas";
     $result = $conn->query($sql);
 
     $tasks = array();
@@ -17,88 +22,95 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                 'id_dentista' => $row['id_dentista'],
                 'titulo' => $row['titulo'],
                 'descripcion' => $row['descripcion'],
-                'imagen_url' => $row['imagen_url']
+                'correo_electronico' => $row['correo_electronico']
             );
         }
     }
 
     $conn->close();
+    header('Content-Type: application/json');
+    echo json_encode($tasks);
+    exit();
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $json_input = file_get_contents('php://input');
+    $data = json_decode($json_input, true);
+
+    $titulo = isset($data['titulo']) ? $data['titulo'] : '';
+    $descripcion = isset($data['descripcion']) ? $data['descripcion'] : '';
+    $correo_electronico = isset($data['correo_electronico']) ? $data['correo_electronico'] : '';
+
+    $stmt = $conn->prepare("INSERT INTO dentistas (titulo, descripcion, correo_electronico) VALUES (?, ? , ?)");
+    $stmt->bind_param("sss", $titulo, $descripcion, $correo_electronico);
+
+    if ($stmt->execute()) {
+        $response = ['message' => 'Datos registrados en la base'];
+    } else {
+        $response = ['error' => 'Error al registrar los datos en la base de datos: ' . $stmt->error];
+    }
+    
+    header('Content-Type: application/json');
+    echo json_encode($response);
+    
+
+} elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    $taskId = isset($_GET['id']) ? $_GET['id'] : '';
+
+    $stmt = $conn->prepare("DELETE FROM dentistas WHERE id_dentista = ?");
+    $stmt->bind_param("i", $taskId);
+
+    $response = ['success' => false, 'message' => 'No se pudo eliminar la tarea'];
+
+    if ($stmt->execute()) {
+        $response['success'] = true;
+        $response['message'] = 'Tarea eliminada exitosamente';
+    } else {
+        $response['error'] = 'Error al eliminar la tarea: ' . $stmt->error;
+    }
+
+    $stmt->close();
 
     header('Content-Type: application/json');
+    echo json_encode($response);
 
-    //----------------------FINAL-------------------------//
 
+} elseif ($_SERVER['REQUEST_METHOD'] === 'HEAD') {
+    $sql = "SELECT COUNT(*) AS total FROM dentistas";
+    $result = $conn->query($sql);
 
-//----------------------METODO POST INICIO-------------------------//
-    echo json_encode($tasks);
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $titulo = $_POST['titulo'];
-    $descripcion = $_POST['descripcion'];
-
-    // Procesar la imagen y guardarla en el servidor
-    $targetDir = '../images/'; // Cambia esto a la ruta donde deseas guardar las imágenes
-    $nombreArchivo = basename($_FILES['imagen']['name']);
-    $targetPath = $targetDir . $nombreArchivo;
-
-    if (move_uploaded_file($_FILES['imagen']['tmp_name'], $targetPath)) {
-        $stmt = $conn->prepare("INSERT INTO dentistas (titulo, descripcion, imagen_url) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $titulo, $descripcion, $targetPath);
-        
-        if ($stmt->execute()) {
-            $response = ['message' => 'Datos registrados en la base de datos y archivo subido exitosamente'];
-        } else {
-            $response = ['error' => 'Error al registrar los datos en la base de datos: ' . $stmt->error];
-        }
-        
-        $stmt->close();
-    } else {
-        $response = ['error' => 'Error al subir el archivo'];
+    $totalDentistas = 0;
+    if ($result) {
+        $row = $result->fetch_assoc();
+        $totalDentistas = $row['total'];
     }
 
     $conn->close();
 
     header('Content-Type: application/json');
+    header('X-Total-Dentistas: ' . $totalDentistas);
+
+    $response = array(
+        'message' => 'Método HEAD ejecutado exitosamente'
+    );
+
     echo json_encode($response);
-}
-//----------------------FINAL-------------------------//
-
-
-//----------------------METODO DELETE INICIO-------------------------//
-if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-    $id = $_GET['id']; // Obtén el ID de la tarea a eliminar
+    http_response_code(200); 
+    exit();
+}elseif ($_SERVER['REQUEST_METHOD'] === 'CONNECT') {
+    $conn->close();
+    echo "Solicitud CONNECT exitosa";
+} elseif ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+    $putData = json_decode(file_get_contents("php://input"), true);
+    $id = isset($putData['id_dentista']) ? $putData['id_dentista'] : '';
+    $titulo = isset($putData['titulo']) ? $putData['titulo'] : '';
+    $descripcion = isset($putData['descripcion']) ? $putData['descripcion'] : '';
+    $correo_electronico = isset($putData['correo_electronico']) ? $putData['correo_electronico'] : '';
+     
     
-    // Realiza la lógica para eliminar la tarea con el ID proporcionado
-    // Por ejemplo:
-    $stmt = $conn->prepare("DELETE FROM dentistas WHERE id_dentista = ?");
-    $stmt->bind_param("i", $id);
+    $stmt = $conn->prepare("UPDATE dentistas SET titulo = ?, descripcion = ?,  correo_electronico = ? WHERE id_dentista = ?");
+    $stmt->bind_param("sssi", $titulo, $descripcion,$correo_electronico, $id);
     
     if ($stmt->execute()) {
-        $response = ['message' => 'Tarea eliminada exitosamente'];
-    } else {
-        $response = ['error' => 'Error al eliminar la tarea: ' . $stmt->error];
-    }
-    
-    $stmt->close();
-
-    header('Content-Type: application/json');
-    echo json_encode($response);
-}
-//----------------------FINAL-------------------------//
-
-
-//----------------------METODO PUT INICIO-------------------------//
-if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
-    $putData = json_decode(file_get_contents("php://input"), true); // Obtén los datos del cuerpo de la petición
-
-    $id = $putData['id_dentista']; // Obtén el ID del dentista a actualizar
-    $titulo = $putData['titulo']; // Obtén el nuevo título del dentista
-    $descripcion = $putData['descripcion']; // Obtén la nueva descripción del dentista
-    
-    $stmt = $conn->prepare("UPDATE dentistas SET titulo = ?, descripcion = ? WHERE id_dentista = ?");
-    $stmt->bind_param("ssi", $titulo, $descripcion, $id);
-    
-    if ($stmt->execute()) {
-        $response = ['message' => 'Tarea actualizada exitosamente'];
+        $response = ['message' => 'Se actualizo correctamente'];
     } else {
         $response = ['error' => 'Error al actualizar la tarea: ' . $stmt->error];
     }
@@ -107,9 +119,55 @@ if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
 
     header('Content-Type: application/json');
     echo json_encode($response);
-
-    //----------------------FINAL-------------------------//
 }
+
+
+function generateCustomReport($id) {
+    include('../conexion/conexion.php');
+    
+    // Depuración: Imprimir el ID recibido
+    echo "ID recibido: $id ";
+    
+    // Consultar los datos en la base de datos utilizando el ID
+    $stmt = $conn->prepare("SELECT id_dentista, titulo, descripcion, correo_electronico FROM dentistas WHERE id_dentista = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    // Verificar si se encontraron resultados
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $customReport = array(
+            'id_dentista' => $row['id_dentista'],
+            'titulo' => $row['titulo'],
+            'descripcion' => $row['descripcion'],
+            'correo_electronico' => $row['correo_electronico']
+        );
+    } else {
+        // No se encontraron resultados para el ID especificado
+        $customReport = array(
+            'error' => 'No se encontraron datos para el ID especificado'
+        );
+    }
+
+    // Cerrar la conexión a la base de datos
+    $stmt->close();
+    $conn->close();
+
+    return $customReport; // Devolver el informe generado
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'REPORT') {
+    // Obtener el ID de informe solicitado (si es necesario)
+    $reportId = isset($_GET['id']) ? $_GET['id'] : '';
+
+    // Generar el informe personalizado basado en el ID
+    $report = generateCustomReport($reportId);
+
+    // Enviar el informe como respuesta
+    header('Content-Type: application/json');
+    echo json_encode($report);
+}
+
 ?>
-
-
